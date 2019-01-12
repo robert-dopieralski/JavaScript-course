89d826d5b1009d4587461eaf86846e9f06c60a93
Robert Dopieralski <abc507446692@gmail.com>
2019-01-12 21:33:06 +0100

    fun with strings!

--- 1/side.js
+++ 1/side.js
@@ -1,8 +1,13 @@
-var a = 6;
-var b = 4;
-
-if (!isNaN(a, b)) {
-
-    console.log("Luckynumber is " + (a * b));
+var string = "I'm a \"fun\" string";
+var a = "string";
+console.log(string);
+console.log(string.toUpperCase());
+console.log(string.indexOf("string"));
+var b = string.indexOf(a);
+console.log(string.indexOf(a));
+if (string.indexOf(a) === -1) {
+    console.log("word " + a + " is not in the string")
 }
-
+else {
+    console.log("the word " + a + " starts at position " + b);
+}
