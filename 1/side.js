5c15aea54bcce416e47b0db371adcc8f9e51faa3
Robert Dopieralski <abc507446692@gmail.com>
2019-01-13 22:44:12 +0100

    'this' function used in objects

--- 1/side.js
+++ 1/side.js
@@ -10,14 +10,41 @@ var myVar = "hey";
 
 myCar.drive();
 
-var myCar2 = { 
+var myCar2 = {
 
-    maxSpeed: 70, 
-    driver: "Hammurabi", 
-    drive: function(speed, time) { 
-        console.log(speed * time); 
+    maxSpeed: 70,
+    driver: "Hammurabi",
+    drive: function (speed, time) {
+        console.log(speed * time);
+    },
+    logDriver: function () {
+        console.log("Driver name is " + this.driver)
+    },
+    test: function () {
+        console.log(this);
     }
 };
 
-console.log(myCar.maxSpeed)
-myCar2.drive(50,3);
+var myCar3 = {
+
+    maxSpeed: 60,
+    driver: "abdul",
+    drive: function (speed, time) {
+        console.log(speed * time);
+    },
+    logDriver: function () {
+        console.log("Driver name is " + this.driver)
+    },
+    test: function () {
+        console.log(this);
+    }
+};
+
+myCar2.logDriver();
+myCar3.logDriver();
+myCar2.test();
+myCar3.test();
+
+console.log(myCar2.maxSpeed)
+myCar2.drive(50, 3);
+
